dfa2593661eb6294208c8c866d532d3e81cfc1b7
g. nicholas d'andrea <gnidan@dramafever.com>
2016-12-21 01:31:34 -0500

    Refactor validity checking to use mapping, not for

--- contracts/RareGem.sol
+++ contracts/RareGem.sol
@@ -25,21 +25,51 @@ contract RareGem {
     bytes32 correctGuessHash;
     mapping(string => bool) priorGuesses;
 
+    mapping(string => bool) allowedGuesses;
+
     uint fee = 1 ether;
 
     event IncorrectGuess(string value);
     event CorrectGuess(string value);
 
+    /*
+     * Constructor
+     */
     function RareGem(bytes32 _correctGuessHash)
-        onlyColorHashes(_correctGuessHash)
+        savingAllowedGuesses()
+        ensureHashIsAllowedGuess(_correctGuessHash)
     {
         correctGuessHash = _correctGuessHash;
     }
 
+    modifier savingAllowedGuesses() {
+        // create fast lookup so that user transactions can be cheaper :0)
+        for (uint i=0; i < colors.length; i++) {
+            allowedGuesses[colors[i]] = true;
+        }
+        _;
+    }
+
+    modifier ensureHashIsAllowedGuess(bytes32 hash) {
+        // no sense in mapping because only constructor needs to check hash
+        bool found = false;
+        for (uint i = 0; i < colors.length; i++) {
+            if (sha3(colors[i]) == hash) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) throw;
+        _;
+    }
+
+    /*
+     * Main public interface
+     */
     function guess(string guess)
         savingGuess(guess)
         onlyNewGuesses(guess)
-        onlyColors(guess)
+        onlyAllowedGuesses(guess)
         requireFee
         notWonYet
         payable
@@ -57,63 +87,24 @@ contract RareGem {
         _;
     }
 
-    modifier onlyNewGuesses(string guess) {
-        if (priorGuesses[guess]) {
-            throw;
-        }
+    modifier requireFee() {
+        if (msg.value < fee) throw;
         _;
     }
 
-    modifier requireFee() {
-        if (msg.value < fee) throw;
+    modifier onlyAllowedGuesses(string guess) {
+        if (!allowedGuesses[guess]) throw;
+        _;
+    }
+
+    modifier onlyNewGuesses(string guess) {
+        if (priorGuesses[guess]) throw;
         _;
     }
 
     modifier savingGuess(string guess) {
         _;
         priorGuesses[guess] = true;
-    }
-
-    modifier onlyColors(string guess) {
-        bool found = false;
-        for (uint i = 0; i < colors.length; i++) {
-            if (stringsEqual(colors[i], guess)) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) throw;
-        _;
-    }
-
-    modifier onlyColorHashes(bytes32 hash) {
-        bool found = false;
-        for (uint i = 0; i < colors.length; i++) {
-            if (sha3(colors[i]) == hash) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) throw;
-        _;
-    }
-
-    function stringsEqual(string storage _a, string memory _b)
-        internal
-        returns (bool)
-    {
-        /* copied from
-         * https://forum.ethereum.org/discussion/3238/string-compare-in-solidity
-         */
-        bytes storage a = bytes(_a);
-        bytes memory b = bytes(_b);
-        if (a.length != b.length)
-            return false;
-        // @todo unroll this loop
-        for (uint i = 0; i < a.length; i ++)
-            if (a[i] != b[i])
-                return false;
-        return true;
     }
 
     event DebugUint(uint value);
